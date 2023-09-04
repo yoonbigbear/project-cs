d789576800fe15e76af2a5ff3619f203bb3efdbf
yoonbigbear <101116747+yoonbigbear@users.noreply.github.com>
2023-09-05 02:05:15 +0900

    TCPServer 추가

---
+++ TcpSession.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Sockets;
+
+namespace Net
+{
+	public class TcpSession : IDisposable
+	{
+		public Socket _socket;
+
+		// Receives
+		SocketAsyncEventArgs _recvEventArg;
+
+		public bool IsSocketDisposed { get; private set; }
+
+		public void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+
+		void Connect(Socket socket)
+		{
+			_socket = socket;
+
+			IsSocketDisposed = false;
+
+		}
+	}
+}
