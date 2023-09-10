36883e66c7b97c974542f2653cbd637db3e2c4da
hugeman091 <hugeman091@gmail.com>
2023-09-11 01:48:48 +0900

    메세지팩 적용 기본 패킷 송수신 완료

---
+++ MessagePack/ChatMP.cs
@@ -0,0 +1,10 @@
+﻿using MessagePack;
+
+[MessagePackObject]
+public class ChatMP
+{
+	[Key(0)]
+	public string Name { get; set; }
+	[Key(1)]
+	public string Message { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,22 +1,28 @@
 ﻿
+using System.Diagnostics;
 using System.Net;
 using System.Net.Http.Headers;
 using System.Net.Sockets;
+using MessagePack;
 using Net;
 
 internal class Program
 {
 	static void Main(string[] args)
 	{
+		string address = "127.0.0.1";
+		int port = 8081;
+
 		if (args.Length > 0)
 		{
 			if (args[0].Equals("server"))
 			{
+
 				TcpServer server = new();
-				server.Tcp(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080));
+				server.Tcp(new IPEndPoint(IPAddress.Parse(address), port));
 
 				server.Start();
-
+				Console.WriteLine("Start Server...");
 				while (true)
 				{
 					Thread.Yield();
@@ -26,16 +32,22 @@ internal class Program
 			}
 			else if (args[0].Equals("client"))
 			{
-				string address = "127.0.0.1";
-				int port = 8080;
-
-				var ep = new IPEndPoint(IPAddress.Parse(address), 8080);
+				var ep = new IPEndPoint(IPAddress.Parse(address), port);
 				TcpClient client = new TcpClient(ep, address, port);
 				client.ConnectAsync();
 
+				Console.WriteLine("Start Client...");
 				while (true)
 				{
-					Thread.Yield();
+					Thread.Sleep(2000);
+
+					var chat = new ChatMP
+					{
+						Name = "client",
+						Message = "Hello server",
+					};
+					var bytes = MessagePackSerializer.Serialize(chat);
+					client.Send(bytes);
 				}
 
 				client.Dispose();
