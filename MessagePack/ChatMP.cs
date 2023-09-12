561c360952eed15f4d7124a7696cdd35e38207ad
hugeman091 <84231954+hugeman091@users.noreply.github.com>
2023-09-13 00:32:02 +0900

    패킷시리얼라이징은 메세지팩 사용.

--- MessagePack/ChatMP.cs
+++ MessagePack/ChatMP.cs
@@ -1,10 +1,22 @@
 ﻿using MessagePack;
+using System;
+
+public interface IPacketHeader
+{
+	public short id { get; set; }
+	public short size { get; set; }
+}
 
 [MessagePackObject]
-public class ChatMP
+public class ChatMP : IPacketHeader
 {
 	[Key(0)]
-	public string Name { get; set; }
+	public short id { get; set; }
 	[Key(1)]
+	public short size { get; set; }
+
+	[Key(2)]
+	public string Name { get; set; }
+	[Key(3)]
 	public string Message { get; set; }
 }
